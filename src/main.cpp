f0a73fdeb2165fd48da72716868ca5a969de8c7c
Stefanie Dargel <nasenbaer8912@gmail.com>
2013-06-25 17:26:44 +0200

    set aboutdata
    Conflicts:
    	CMakeLists.txt
    	src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <klocalizedstring.h>
+#include <kaboutdata.h>
 #include <QApplication>
 // #include <QtQuick/QQuickView>
 // #include <QtQml/QQmlEngine>
@@ -5,7 +7,25 @@
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
+    KAboutData aboutData("krecipes2",               //componentName
+                         0,                         //catalogName
+                         i18n("Krecipes2"),        //displayName
+                         "1.0",                     //version
+                         i18n("The KDE Cookbook"), //shortDescription
+                         KAboutData::License_GPL,   //licenseType
+                         i18n("(c) 2013"),         //copyrightStatement
+                         // Optional text shown in the About box.
+                         // Can contain any information desired.
+                         i18n("This product is RecipeML compatible.\nYou can get more information about this file"
+                         "format in:\nhttp://www.formatdata.com/recipeml"),
+                         "http://example.com/",     //homePageAddress
+                         "submit@bugs.kde.org");    //bugsEmailAddress
+
     QApplication app(argc, argv);
+    app.setApplicationName(aboutData.componentName());
+    app.setApplicationVersion(aboutData.catalogName());
+    app.setOrganizationDomain(aboutData.homepage());
+    app.setApplicationDisplayName(aboutData.displayName());
 
     Krecipes2 * widget = new Krecipes2;
     widget->show();
