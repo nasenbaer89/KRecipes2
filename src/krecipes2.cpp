cef99c07a15edcf822f642a728c0299663e0bc5c
Stefanie Dargel <nasenbaer8912@gmail.com>
2013-06-26 16:25:00 +0200

    clean-up

--- src/krecipes2.cpp
+++ src/krecipes2.cpp
@@ -1,8 +1,6 @@
 #include <QtWidgets>
-#include <QSqlError>
-#include <QSqlQuery>
 #include <iostream>
-#include <kdebug.h>
+#include <qdebug.h>
 
 #include "krecipes2.h"
 #include "backends/recipedb.h"
@@ -10,32 +8,24 @@
 Krecipes2::Krecipes2(QWidget* parent): QWidget(parent)
 {
     qDebug();
-    RecipeDB* db = RecipeDB::createDatabase("SQLite");
-    db->connect();
-
-//     query.exec("create table images (locationid int, file varchar(20))");
-//     query.exec("insert into images values(0, 'images/oslo.png')");
-//     query.exec("insert into images values(1, 'images/brisbane.png')");
-//     query.exec("insert into images values(2, 'images/redwood.png')");
-//     query.exec("insert into images values(3, 'images/berlin.png')");
-//     query.exec("insert into images values(4, 'images/munich.png')");
-//     query.exec("insert into images values(5, 'images/beijing.png')");
+//     RecipeDB* db = RecipeDB::createDatabase("SQLite");
+//     db->connect();
     
-    createButton = new QPushButton(tr("Create Database"));
-    createButton->setDefault(true);
-    
-    quitButton = new QPushButton(tr("Quit"));
-
-    buttonBox = new QDialogButtonBox(Qt::Vertical);
-    buttonBox->addButton(createButton, QDialogButtonBox::AcceptRole);
-    buttonBox->addButton(quitButton, QDialogButtonBox::RejectRole);
-
-    connect(quitButton, SIGNAL(clicked()), this, SLOT(close()));
-//     connect(createButton, SIGNAL(clicked()), this, SLOT(create_tables()));
-
-    QHBoxLayout *mainLayout = new QHBoxLayout;
-    mainLayout->addWidget(buttonBox);
-    setLayout(mainLayout);
+//     createButton = new QPushButton(tr("Create Database"));
+//     createButton->setDefault(true);
+//     
+//     quitButton = new QPushButton(tr("Quit"));
+// 
+//     buttonBox = new QDialogButtonBox(Qt::Vertical);
+//     buttonBox->addButton(createButton, QDialogButtonBox::AcceptRole);
+//     buttonBox->addButton(quitButton, QDialogButtonBox::RejectRole);
+// 
+//     connect(quitButton, SIGNAL(clicked()), this, SLOT(close()));
+// //     connect(createButton, SIGNAL(clicked()), this, SLOT(create_tables()));
+// 
+//     QHBoxLayout *mainLayout = new QHBoxLayout;
+//     mainLayout->addWidget(buttonBox);
+//     setLayout(mainLayout);
 
 //     setWindowTitle(tr("Cached Table"));
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 #include <QApplication>
-#include <QSqlDatabase>
-#include <QtQuick/QQuickView>
-#include <QtQml/QQmlEngine>
+// #include <QtQuick/QQuickView>
+// #include <QtQml/QQmlEngine>
 #include "krecipes2.h"
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    Krecipes2 widget;
-    widget.show();
+    Krecipes2 * widget = new Krecipes2;
+    widget->show();
 //     QQuickView viewer;
 //     viewer.setSource(QUrl("../src/qml/main.qml"));
 //     viewer.show();
