5202a9262089bc8f212a5b9ff4817d1780551f42
Stefanie Dargel <nasenbaer8912@gmail.com>
2013-07-19 19:27:17 +0200

    fix exit on last window closed

--- src/krecipes2.cpp
+++ src/krecipes2.cpp
@@ -13,6 +13,8 @@
 
 Krecipes2::Krecipes2(QWidget *parent) : KXmlGuiWindow(parent)
 {
+    qApp->setQuitOnLastWindowClosed(true);
+
     m_view = new Krecipes2View(this);
     setObjectName("Krecipes2");
     // accept dnd
